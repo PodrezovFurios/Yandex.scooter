a972b603a1e85ed3ce081bacb5c2959889c2cc6e
Your Name <you@example.com>
2023-07-15 11:01:37 +0300

    Andrey

--- assert.py
+++ assert.py
@@ -3,3 +3,4 @@ import sender_stand_request
 def test_Check_that_the_response_code_is_200():
     assert sender_stand_request.get_orders_track().status_code == 200
 
+# Подрезов Андрей, 6-я когрорта финальный проект
